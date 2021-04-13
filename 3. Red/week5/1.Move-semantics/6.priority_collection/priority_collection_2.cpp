c74f49b2f0a33b3e0095f0e8b1218ba84c467005
ChukCharles <chuk.charles@gmail.com>
2021-04-13 23:37:15 +0300

    Update priority_collection_2.cpp

--- 3. Red/week5/1.Move-semantics/6.priority_collection/priority_collection_2.cpp
+++ 3. Red/week5/1.Move-semantics/6.priority_collection/priority_collection_2.cpp
@@ -6,30 +6,23 @@
 #include <set>
 #include <utility>
 #include <vector>
-
 #include <map>
-#include <deque>
+#include <string>
 
 using namespace std;
-
 template <typename T>
 class PriorityCollection {
 public:
-    using Id = typename deque<T>::iterator;
-
-    PriorityCollection() : max_pr(0) {}
+    using Id = int;
 
     // Добавить объект с нулевым приоритетом
     // с помощью перемещения и вернуть его идентификатор
     Id Add(T object) {
-        objects.push_back(move(object));
-        Id tmp = --objects.end();
-        priorById[tmp] = 0;
-        idByPrior[0].insert(tmp);
-        if( max_pr == 0 ) {
-            max_id = tmp;
-        }
-        return tmp;
+        Id idx = data_.size();
+        data_.push_back(move(object));
+        priority_by_id_[idx] = 0;
+        element_by_proirity_[0].insert(idx);
+        return idx;
     }
 
     // Добавить все элементы диапазона [range_begin, range_end)
@@ -37,113 +30,150 @@ public:
     // в диапазон [ids_begin, ...)
     template <typename ObjInputIt, typename IdOutputIt>
     void Add(ObjInputIt range_begin, ObjInputIt range_end,
-            IdOutputIt ids_begin) {
-        int before_size = objects.size();
-        copy(make_move_iterator(range_begin), make_move_iterator(range_end),
-             back_inserter(objects));
-        for( auto it = objects.begin()+before_size; it != objects.end(); ++it) {
-            priorById[it] = 0;
-            idByPrior[0].insert(it);
-            if( max_pr == 0 ) {
-                max_id = it;
-            }
-            *ids_begin = it;
-            ids_begin++;
+        IdOutputIt ids_begin) {
+        Id first_idx = data_.size();
+        ObjInputIt it = range_begin;
+        while (it != range_end) {
+            Id idx = data_.size();
+            *(ids_begin++) = idx;
+            priority_by_id_[idx] = 0;
+            element_by_proirity_[0].insert(idx);
+
+            data_.push_back(move(*it++));
         }
     }
 
     // Определить, принадлежит ли идентификатор какому-либо
     // хранящемуся в контейнере объекту
     bool IsValid(Id id) const {
-        return priorById.count(id);
+        if (priority_by_id_.count(id)>0) return true;
+        return false;
     }
 
     // Получить объект по идентификатору
     const T& Get(Id id) const {
-        return (*id);
+        return data_[id];
     }
 
     // Увеличить приоритет объекта на 1
     void Promote(Id id) {
-        int old_pr = priorById[id];
-        idByPrior[old_pr].erase(id);
-        idByPrior[++old_pr].insert(id);
-        priorById[id]++;
-        if ( old_pr > max_pr ||
-             ( old_pr == max_pr && id > max_id )) {
-            max_pr = old_pr;
-            max_id = id;
-        }
+        int old_pr = priority_by_id_[id];
+        auto it = find(element_by_proirity_[old_pr].begin(),
+                       element_by_proirity_[old_pr].end(),
+                       id);
+
+        element_by_proirity_[old_pr].erase(it);
+        if (element_by_proirity_[old_pr].size() == 0) element_by_proirity_.erase(old_pr);
+        int new_pr = ++old_pr;
+        element_by_proirity_[new_pr].insert(id);
+        priority_by_id_[id] = new_pr;
     }
 
     // Получить объект с максимальным приоритетом и его приоритет
     pair<const T&, int> GetMax() const {
-        return { *max_id, max_pr };
+        auto max = --element_by_proirity_.end();
+        int max_priority = max->first;
+        int max_element_id = *(--max->second.end());
+        return { data_[max_element_id], max_priority };
     }
 
     // Аналогично GetMax, но удаляет элемент из контейнера
     pair<T, int> PopMax() {
-        pair<T, int> res(move(*max_id), max_pr);
-        auto max_it = --idByPrior[max_pr].end();
-        idByPrior[max_pr].erase(max_it);
-        priorById.erase(max_id);
-        while ( idByPrior[max_pr].size() == 0 && max_pr > 0 ) {
-            max_pr -= 1;
-        }
-        if( idByPrior[max_pr].size() > 0 ) {
-            max_id = *(--(idByPrior[max_pr].end()));
-        }
+        auto max = --element_by_proirity_.end();
+        int max_priority = max->first;
+        int max_element_id = *(--max->second.end());
 
-        return res;
+        priority_by_id_.erase(max_element_id);
+        element_by_proirity_[max_priority].erase(max_element_id);
+        if (element_by_proirity_[max_priority].size() == 0) element_by_proirity_.erase(max_priority);
+
+        return make_pair(move(data_[max_element_id]), max_priority);
     }
 
 private:
-    deque<T> objects;
-    map<Id, int> priorById;
-    map<int, set<Id>> idByPrior;
-    int max_pr;
-    Id max_id;
+    vector<T> data_;
+    map<Id, int> priority_by_id_;
+    map<int, set<Id>> element_by_proirity_;
 };
+
 
 class StringNonCopyable : public string {
 public:
-    using string::string; // Позволяет использовать конструкторы строки
-    StringNonCopyable(const StringNonCopyable&) = delete;
-    StringNonCopyable(StringNonCopyable&&) = default;
-    StringNonCopyable& operator=(const StringNonCopyable&) = delete;
-    StringNonCopyable& operator=(StringNonCopyable&&) = default;
+  using string::string;  // Позволяет использовать конструкторы строки
+  StringNonCopyable(const StringNonCopyable&) = delete;
+  //StringNonCopyable(const StringNonCopyable&) { 
+  //    cout << "copy c-tor" << endl;
+  //};
+  StringNonCopyable(StringNonCopyable&&) = default;
+  StringNonCopyable& operator=(const StringNonCopyable&) = delete;
+  //StringNonCopyable& operator=(const StringNonCopyable&) { cout << "operator =" << endl; };
+  StringNonCopyable& operator=(StringNonCopyable&&) = default;
 };
 
 void TestNoCopy() {
-    PriorityCollection<StringNonCopyable> strings;
-    const auto white_id = strings.Add("white");
-    const auto yellow_id = strings.Add("yellow");
-    const auto red_id = strings.Add("red");
+  PriorityCollection<StringNonCopyable> strings;
+  const auto white_id = strings.Add("white");
+  const auto yellow_id = strings.Add("yellow");
+  const auto red_id = strings.Add("red");
 
-    strings.Promote(yellow_id);
-    for (int i = 0; i < 2; ++i) {
-        strings.Promote(red_id);
-    }
-    strings.Promote(yellow_id);
-    {
-        const auto item = strings.PopMax();
-        ASSERT_EQUAL(item.first, "red");
-        ASSERT_EQUAL(item.second, 2);
-    }
-    {
-        const auto item = strings.PopMax();
-        ASSERT_EQUAL(item.first, "yellow");
-        ASSERT_EQUAL(item.second, 2);
-    }
-    {
-        const auto item = strings.PopMax();
-        ASSERT_EQUAL(item.first, "white");
-        ASSERT_EQUAL(item.second, 0);
-    }
+  strings.Promote(yellow_id);
+  for (int i = 0; i < 2; ++i) {
+    strings.Promote(red_id);
+  }
+  strings.Promote(yellow_id);
+  {
+    const auto item = strings.PopMax();
+    ASSERT_EQUAL(item.first, "red");
+    ASSERT_EQUAL(item.second, 2);
+  }
+  {
+    const auto item = strings.PopMax();
+    ASSERT_EQUAL(item.first, "yellow");
+    ASSERT_EQUAL(item.second, 2);
+  }
+  {
+    const auto item = strings.PopMax();
+    ASSERT_EQUAL(item.first, "white");
+    ASSERT_EQUAL(item.second, 0);
+  }
+}
+
+void TestConstructionFromRange() {
+    PriorityCollection<StringNonCopyable> strings;
+    vector<StringNonCopyable> source;
+    source.push_back("white");
+    source.push_back("yellow");
+    source.push_back("red");
+    vector<PriorityCollection<StringNonCopyable>::Id> ids(3);
+
+
+    strings.Add(source.begin(), source.end(), ids.begin());
+    //for (auto id : ids) cout << id << " ";
+    strings.Promote(1);
+    //strings.GetMax();
+    //cout << max.second << endl;
 }
 
 int main() {
-    TestRunner tr;
-    RUN_TEST(tr, TestNoCopy);
+    //TestRunner tr;
+    //RUN_TEST(tr, TestNoCopy);
+    //RUN_TEST(tr, TestConstructionFromRange);
+    auto collection = std::make_unique<PriorityCollection<StringNonCopyable>>();
+    std::vector<StringNonCopyable> arr;
+    arr.push_back("0");
+    arr.push_back("1");
+    arr.push_back("2");
+    arr.push_back("3");
+
+    using ID = PriorityCollection<StringNonCopyable>::Id;
+    std::vector<ID> id_arr;
+    collection->Add(arr.begin(), arr.end(), std::back_inserter(id_arr));
+    //std::cout << id_arr.size() << '\n';
+    //for (auto x : id_arr) {
+    //    cout << x << " " << endl;
+    //}
+
+    auto maximum = collection->GetMax();
+    std::cout << maximum.first << ' ' << maximum.second << '\n';
     return 0;
 }
